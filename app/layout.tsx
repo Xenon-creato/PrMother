871b7e8d5ace3b79665220b741a495e9a64c9df7
Your Name <your@email.com>
2026-03-17 19:04:08 +0200

    fix meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,11 @@ import { Manrope } from "next/font/google";
 import "./globals.css";
 import SiteHeader from "@/components/site-header";
 import Script from "next/script";
+
 const manrope = Manrope({
   subsets: ["latin"],
 });
+
 export const metadata: Metadata = {
   title: "Формула любові",
   description: "Онлайн-школа Ірини Табаки",
@@ -18,28 +20,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
-      <head>
+      <body className={`${manrope.className} antialiased`}>
+        <SiteHeader />
+        {children}
+
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
             fbq('init', '1161478935965119');
             fbq('track', 'PageView');
           `}
         </Script>
-      </head>
-      
-      <body
-        className={`${manrope.className} antialiased`}
-      >
-        <SiteHeader />
-        {children}
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1161478935965119&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
