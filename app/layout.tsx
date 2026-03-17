e1e09b2fb07c2b67f1bd79734272351b2f7d197e
Your Name <your@email.com>
2026-03-17 18:36:32 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '889324683922599');
+            fbq('init', '1161478935965119');
             fbq('track', 'PageView');
           `}
         </Script>
